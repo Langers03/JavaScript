a562e65715d1a5b103281421345694bd920414c7
Langers03 <Jakelan2003@gmail.com>
2026-02-12 15:37:47 +0000

    Add files via upload
    This is All the coding challenges from my Second Module in JavaScript Featuring Loops, Arrays, Variables and Conditional Statements

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,14 @@
-function changeHeadingText () {
+function changeHeadingText () { //this is to show onclick property and variable use.
     var headingelement = document.getElementById("heading")
     
     headingelement.textContent = "I Told You!";
 }
+
+console.log("This Is a message for the console!") //this is to show Console messages
+
+
+let products = ["Games", "CDS", "DVDS", "Vinyl", "Books"]; //This is a Array
+
+for (let i = 0; i < products.length; i++) {
+    console.log("Products", products[i]); 
+} //This is a Loop for String Array
